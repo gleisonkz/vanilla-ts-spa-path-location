53ff69509ba7ccd391618d4d6ea8a861798b8250
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-09-10 02:38:21 -0300

    📦 refactor: change api to animes service class

---
+++ src/classes/animes.service.js
@@ -0,0 +1,28 @@
+const BASE_URL = "https://animechan.vercel.app/api";
+
+export class AnimesService {
+  static async getAnimes() {
+    const cachedAnimes = JSON.parse(localStorage.getItem("animes")) || [];
+
+    if (cachedAnimes.length > 0) return cachedAnimes;
+
+    const response = await fetch(`${BASE_URL}/available/anime`);
+    const animes = await response.json();
+    localStorage.setItem("animes", JSON.stringify(animes));
+    return animes;
+  }
+
+  static async getAnimeQuotes(animeName) {
+    if (!animeName) throw new Error("Anime name is required");
+
+    const cachedQuotes = JSON.parse(localStorage.getItem(animeName)) || [];
+
+    if (cachedQuotes.length > 0) return cachedQuotes;
+
+    const response = await fetch(`${BASE_URL}/quotes/anime?title=${animeName}`);
+    const quotes = await response.json();
+
+    localStorage.setItem(animeName, JSON.stringify(quotes));
+    return quotes;
+  }
+}
