d41efc46d9418a26c35c52a739958ac2612a20bc
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-09-10 06:31:26 -0300

    ✨ feat: block ui service

---
+++ src/classes/block-ui.service.ts
@@ -0,0 +1,53 @@
+const BASE_URL = "https://animechan.vercel.app/api";
+
+export class BlockUIService {
+  private static _instance: BlockUIService;
+  private $overlay: HTMLDivElement = this.createLoadScreen();
+
+  private constructor() {}
+
+  public static get instance(): BlockUIService {
+    const hasInstance = !!BlockUIService._instance;
+    return !hasInstance
+      ? (BlockUIService._instance = new BlockUIService())
+      : BlockUIService._instance;
+  }
+
+  createLoadScreen(): HTMLDivElement {
+    const $styles = document.createElement("style");
+    $styles.textContent = `
+      .loading-overlay {
+        display: flex;
+        z-index: 99999;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background: rgba(0, 0, 0, .6);
+        justify-content: center;
+        align-items: center;
+        color: white;
+        font-size: 40px;
+    } 
+      `;
+
+    const $loadingOverlay = document.createElement("div");
+    $loadingOverlay.classList.add("loading-overlay");
+    $loadingOverlay.textContent = "loading...";
+    $loadingOverlay.appendChild($styles);
+
+    document.body.appendChild($loadingOverlay);
+    return $loadingOverlay;
+  }
+
+  show(): void {
+    console.log("show");
+    this.$overlay.style.display = "flex";
+  }
+
+  hide(): void {
+    window.scrollTo(0, 0);
+    this.$overlay.style.display = "none";
+  }
+}
