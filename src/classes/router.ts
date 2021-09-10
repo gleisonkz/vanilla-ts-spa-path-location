511ce69ab63ac29216e74825e803430132463efa
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-09-10 05:33:06 -0300

    📦 refactor: extract router to its own file

---
+++ src/classes/router.ts
@@ -0,0 +1,61 @@
+import { AboutPage } from '../pages/about.page';
+import { AnimesPage } from '../pages/animes.page';
+import { HomePage } from '../pages/home.page';
+
+const ROUTES = [
+  { path: "/", page: HomePage },
+  { path: "/animes", page: AnimesPage },
+  // { path: "/posts/:id", page: PostView },
+  { path: "/about", page: AboutPage },
+];
+
+export class Router {
+  private static _instance: Router;
+  private static $app = document.querySelector("#app");
+
+  private constructor() {}
+
+  public static get instance(): Router {
+    const hasRouter = !!Router._instance;
+    return !hasRouter ? (Router._instance = new Router()) : Router._instance;
+  }
+
+  private get $app(): Element {
+    if (!Router.$app) throw new Error("No app element found");
+    return Router.$app;
+  }
+
+  async watch(): Promise<void> {
+    let matchedRoute = ROUTES.map((route) => {
+      const pattern = this.pathToRegexPattern(route.path);
+      const currentPath = location.pathname;
+
+      const match = currentPath.match(pattern);
+
+      return { route, match };
+    }).find((route) => route.match);
+
+    if (!matchedRoute) {
+      matchedRoute = {
+        route: ROUTES[0],
+        match: [location.pathname],
+      };
+    }
+
+    const params = "";
+    const Page = matchedRoute.route.page;
+    const pageObj = new Page(params);
+    const $page = await pageObj.getPage();
+    this.updateApp($page);
+  }
+
+  updateApp($element: HTMLElement) {
+    this.$app.innerHTML = "";
+    const $page = $element;
+    this.$app.appendChild($page);
+  }
+
+  pathToRegexPattern(path: string): string {
+    return path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$";
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,16 @@
-import { AboutPage } from './pages/about.page';
-import { AnimesPage } from './pages/animes.page';
-import { HomePage } from './pages/home.page';
-
-const $app = document.querySelector("#app");
-
-const ROUTES = [
-  { path: "/", page: HomePage },
-  { path: "/animes", page: AnimesPage },
-  // { path: "/posts/:id", page: PostView },
-  { path: "/about", page: AboutPage },
-];
-
-const pathToRegexPattern = (path: string) =>
-  path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$";
+import { Router } from './classes/router';
 
 const navigateTo = (path: string) => {
   history.pushState(null, "", path);
-  router();
-};
-
-const updateApp = ($element: HTMLElement) => {
-  if (!$app) throw new Error("No app element found");
-
-  $app.innerHTML = "";
-  const $page = $element;
-  $app?.appendChild($page);
-};
-
-const router = async () => {
-  // Test each route for a potential match.
-  let targetRoute = ROUTES.map((route) => {
-    const pattern = pathToRegexPattern(route.path);
-    const currentPath = location.pathname;
-
-    const match = currentPath.match(pattern);
-
-    return { route, match };
-  }).find((route) => route.match);
-
-  if (!targetRoute) {
-    targetRoute = {
-      route: ROUTES[0],
-      match: [location.pathname],
-    };
-  }
-
-  const params = "";
-  const Page = targetRoute.route.page;
-  const pageObj = new Page(params);
-  const $page = await pageObj.getPage();
-  updateApp($page);
+  Router.instance.watch();
 };
 
 const setAnchorListener = (event: Event) => {
   event.preventDefault();
   const element = event.target as HTMLElement;
   const hasDataLink = element.matches("[router-link]");
-  console.log(element);
+
   if (!hasDataLink) return;
-  console.log(element);
 
   const path = element.getAttribute("router-link");
   if (!path) throw new Error("No path found");
@@ -68,5 +20,5 @@ const setAnchorListener = (event: Event) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.addEventListener("click", setAnchorListener);
-  router();
+  Router.instance.watch();
 });
